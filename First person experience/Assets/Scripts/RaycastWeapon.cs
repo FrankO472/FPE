c7bd8522e175971bf03bfe70267b3ef3f1b7cc47
FrankO472 <mediumlister472@gmail.com>
2024-10-15 12:16:01 +0100

    gun update
    got a gun and targets

---
+++ First person experience/Assets/Scripts/RaycastWeapon.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastWeapon : MonoBehaviour
+{
+
+    public int damage;
+    public float shootDistance;
+    public LayerMask layerMask;
+    public GameObject cam;
+    RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, shootDistance, layerMask))
+                {
+                    hit.collider.gameObject.GetComponent<Health>().hp -= damage;
+                }
+            }
+        }
+    }
+}
