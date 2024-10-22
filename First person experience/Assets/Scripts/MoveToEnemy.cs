9189c21d35c76c63fc8da74ce8f6d13c258f59b2
FrankO472 <mediumlister472@gmail.com>
2024-10-22 12:11:43 +0100

    M&R enemies
    yeah

---
+++ First person experience/Assets/Scripts/MoveToEnemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToEnemy : MonoBehaviour
+{
+    public float damageAmount, attackDelay, attackRate, attackDistance, health;
+
+    GameObject playerObject;
+
+    NavMeshAgent navAgent;
+    public Transform bulletPos;
+    public GameObject bullet; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerObject = GameObject.FindWithTag("Player");
+        navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(Time.time > attackDelay)
+        {
+            if(Vector3.Distance(playerObject.transform.position, transform.position) <= attackDistance)
+            {
+             Attack();
+            }
+        }
+    }
+
+    void Move()
+    {
+        navAgent.destination = playerObject.transform.position;
+    }
+
+    void Attack()
+    {
+         playerObject.GetComponent<PlayerHealth>().health -= damageAmount;
+         attackDelay = Time.time + attackRate;
+        //Instantiate (bullet, bulletPos.position, transform.rotation);
+    }
+
+
+}
